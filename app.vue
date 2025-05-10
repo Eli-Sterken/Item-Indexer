d365150d48e53c2a64c02c88477e55b5582c0065
Eli Sterken <eli.sterken@outlook.com>
2025-05-10 13:42:42 -0700

    Added Meta Added title and meta information.

--- app.vue
+++ app.vue
@@ -2,3 +2,23 @@
     <h1 class="text-4xl rainbow-text text-center font-bold mb-[3%]">In Development</h1>
     <h1 class="text-4xl rainbow-text text-center font-bold">Item Indexer Is Still Under Development, Please Check Back Soon!</h1>
 </template>
+
+<script setup lang="ts">
+    // SEO and meta
+
+    useHead({
+        title: 'Item Indexer - In Development'
+    });
+
+    useSeoMeta({
+        ogType: 'website',
+        ogUrl: 'https://item-indexer.nuxt.dev',
+        ogTitle: 'Item Indexer',
+        ogDescription: 'An app to store items with a numeric code',
+        ogImage: 'https://raw.githubusercontent.com/Eli-Sterken/Item-Indexer/refs/heads/main/.github/images/Logo.png',
+        twitterCard: 'summary_large_image',
+        twitterTitle: 'Item Indexer',
+        twitterDescription: 'An app to store items with a numeric code',
+        twitterImage: 'https://raw.githubusercontent.com/Eli-Sterken/Item-Indexer/refs/heads/main/.github/images/Logo.png'
+    });
+</script>
