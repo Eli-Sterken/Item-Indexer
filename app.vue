ea1375153f88c8de341489dd6d52c6f76eb91c9a
Eli Sterken <eli.sterken@outlook.com>
2025-05-10 13:27:14 -0700

    Added Structure
    Added the inital project structure.

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-4xl rainbow-text text-center font-bold mb-[3%]">In Development</h1>
+    <h1 class="text-4xl rainbow-text text-center font-bold">Item Indexer Is Still Under Development, Please Check Back Soon!</h1>
+</template>
